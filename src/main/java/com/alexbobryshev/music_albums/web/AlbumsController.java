1d71a64f3bedf555b9b526c3e461e0e6b49fb37e
AlexBobryshev <darknessrider1@rambler.ru>
2019-08-21 10:16:41 +0300

    1st commit

---
+++ src/main/java/com/alexbobryshev/music_albums/web/AlbumsController.java
@@ -0,0 +1,14 @@
+package com.alexbobryshev.music_albums.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+/*
+@Controller
+@RequestMapping("/")
+public class AlbumsController {
+    @GetMapping
+    public String showGreetingsPage() {
+        return "home";
+    }
+}*/
